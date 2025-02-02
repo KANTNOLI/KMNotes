c4c281838f1c0e66646a44692ca9cb18cfe72712
KANTNOLI <zahargysev02@gmail.com>
2025-02-03 01:04:54 +0300

    Starting learning React Native - Test  Mobile App

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
 
 export default function App() {
+  let testString = "test";
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text
+        numberOfLines={1}
+        onPress={() => {
+          testString += " 1";
+        }}
+      >
+        123 {"\n"} 123
+      </Text>
+
+      <Button title="12323" />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff111",
   },
 });
